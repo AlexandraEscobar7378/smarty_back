62732594fb170a1cadb497f2cbea02bdc8d4ec90
Iris Minerva Peraza Meléndez <123664025+IrisMinervaMelendez@users.noreply.github.com>
2023-04-12 12:03:47 -0600

    Actualización de controldor de usuarios
    Se agregaron métodos update, delete, manage & create...
    FALTA QUE AGREGAR METODOS EN RUTA DE ARCHIVO API!!!
    
    Falta que corroborar estos campos en tabla y vitas de Front

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -6,5 +6,80 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for create a new user for the new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function create_newUser()    ////FALTA AGREGAR METODO POST A RUTAS EN API!!!!
+    {
+        <form action="{{ route('users.create') }}" method="post">
+        @csrf
+        <label for="name">Nombre:</label>
+        <input type="text" name="name" id="name">
+        <label for="email">Correo electrónico:</label>
+        <input type="email" name="email" id="email">
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Agregar usuario</button>
+        </form>
+        }
+   
+    
+    
+    /**
+     * Show the form for manage the new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+
+    public function manageUser()
+    {
+        $users = User::all(); // Obtener todos los usuarios
+        return view('users.manage', compact('users'));
+    }
+
+    
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    
+     */
+    public function update(Request $request, $id) //// AGREGAR EL METODO UPDATE O POST A LAS RUTAAS EN ARCHIVO API!!!
+    {
+        $user = User::find($id);
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+
+        return redirect('/users')->with('success', 'El usuario ha sido actualizado exitosamente.');
+    }
+
+
+         
+        
+    public function deleteUser($id)  ////AGREGAR EL METODO DELETE A LAS RUTAS EN ARCHIVO API
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect('/users')->with('success', 'El usuario ha sido eliminado exitosamente.');
+    }
+
 }
